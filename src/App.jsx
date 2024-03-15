7bd5ef03d6fe5e851707678eb709bfd44f2daa33
FuadMansurov <fuadmansurov2020@gmail.com>
2024-03-15 14:59:12 -0400

    blog first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+import CreatePost from './components/CreatePost'
+
+function App() {
+
+  return (
+    <>
+      <CreatePost />
+      <Card/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,11 @@
+function CreatePost (){
+    return (
+        <>
+            <h1>New Post</h1>
+            <input type="text" />
+            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <button>create</button>
+        </>
+    )
+}
+export default CreatePost
